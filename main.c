2dcefdf293155ce999f6d187d0ff6cadbd023830
Rasmus Severinsen <rsever22@student.aau.dk>
2022-10-28 10:36:33 +0200

    Initial

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    char test = 'a';
+
     printf("Hello, World!\n");
+    scanf(" %c", test);
+    if (test == 'a') {
+        printf("Test is 1");
+    } else {
+        printf("Test is %d", test);
+    }
     return 0;
 }
