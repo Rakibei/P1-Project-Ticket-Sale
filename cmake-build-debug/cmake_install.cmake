c3f540b18f6c1453a88bf61ea5c9aabffed1f229
alexaaskov <EQ15JQ@student.aau.dk>
2022-10-28 11:22:09 +0200

    Alexander made a test!

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Rakib/CLionProjects/P1-Project-Ticket-Sale
+# Install script for directory: D:/Desktop/Software/P1/Ticket_Sale
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "D:/JetBrains/CLion 2022.2.1/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Rakib/CLionProjects/P1-Project-Ticket-Sale/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Desktop/Software/P1/Ticket_Sale/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
